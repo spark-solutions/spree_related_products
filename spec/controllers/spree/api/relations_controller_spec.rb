8f0492c6c5f8185bf99cc8763c704685d52a4ba5
symarys <maria.syczewska@sparksolutions.co>
2017-07-26 17:02:47 +0200

    Fix params in controller specs

--- spec/controllers/spree/api/relations_controller_spec.rb
+++ spec/controllers/spree/api/relations_controller_spec.rb
@@ -37,12 +37,12 @@ RSpec.describe Spree::Api::RelationsController, type: :controller do
 
     context '#create' do
       it 'creates the relation' do
-        spree_post :create, valid_params
+        post :create, params: valid_params
         expect(response.status).to eq(201)
       end
 
       it 'responds 422 error with invalid params' do
-        spree_post :create, format: :json, product_id: product.id, token: user.spree_api_key
+        post :create, params: { format: :json, product_id: product.id, token: user.spree_api_key }
         expect(response.status).to eq(422)
       end
     end
@@ -51,7 +51,7 @@ RSpec.describe Spree::Api::RelationsController, type: :controller do
       it 'succesfully updates the relation ' do
         params = { format: :json, product_id: product.id, id: relation.id, relation: { discount_amount: 2.0 }, token: user.spree_api_key }
         expect {
-          spree_put :update, params
+          put :update, params: params
         }.to change { relation.reload.discount_amount.to_s }.from('0.0').to('2.0')
       end
     end
@@ -59,7 +59,7 @@ RSpec.describe Spree::Api::RelationsController, type: :controller do
     context '#destroy with' do
       it 'records successfully' do
         expect {
-          spree_delete :destroy, id: relation.id, product_id: product.id, format: :json, token: user.spree_api_key
+          delete :destroy, params: { id: relation.id, product_id: product.id, format: :json, token: user.spree_api_key }
         }.to change(Spree::Relation, :count).by(-1)
       end
     end
@@ -70,7 +70,7 @@ RSpec.describe Spree::Api::RelationsController, type: :controller do
         relation2 = create(:relation, relatable: product, related_to: other2, relation_type: relation_type, position: 1)
 
         expect {
-          spree_post :update_positions, product_id: product.id, id: relation.id, positions: { relation.id => '1', relation2.id => '0' }, format: :json, token: user.spree_api_key
+          post :update_positions, params: { product_id: product.id, id: relation.id, positions: { relation.id => '1', relation2.id => '0' }, format: :json, token: user.spree_api_key }
           relation.reload
         }.to change(relation, :position).from(0).to(1)
       end
